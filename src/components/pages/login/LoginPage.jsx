9d5ebe6e6979fb2e45f04b0f842a4f041ecb094b
Jason.renault <55652225+jasonrenau@users.noreply.github.com>
2024-01-01 16:53:27 +0100

    Add login form styling and logo component

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,9 +1,10 @@
 import LoginForm from "./LoginForm";
+import Logo from "./Logo";
 
 const LoginPage = () => {
   return (
-    <main>
-      {/* <Logo/> */}
+    <main className="flex h-dvh flex-col items-center justify-center bg-[url('src/assets/images/background.jpg')] bg-cover bg-center bg-no-repeat bg-blend-luminosity">
+      <Logo />
       <LoginForm />
     </main>
   );
