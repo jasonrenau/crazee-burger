2711d0f3eb5fbfcf5327c1d55c87d32c58b582b3
Jason.renault <55652225+jasonrenau@users.noreply.github.com>
2024-01-14 13:15:31 +0100

    Add useNavigate hook to NavbarLogo component

--- src/components/ui/NavbarLogo.jsx
+++ src/components/ui/NavbarLogo.jsx
@@ -1,11 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 
 export default function NavbarLogo() {
+  const navigate = useNavigate();
+
   return (
-    <div
-      className="flex cursor-pointer"
-      onClick={() => window.location.reload()}
-    >
+    <div className="flex cursor-pointer" onClick={() => navigate(0)}>
       <h2 className="text-4xl text-yellow-500 ">CRAZEE</h2>
       <img src={logo} alt="logo" className="mx-auto size-10 " />
       <h2 className="text-4xl text-yellow-500 ">BURGER</h2>
