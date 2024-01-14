fa871a2f8d6eb41ff9142dba4e939d8c09df3fce
Jason.renault <55652225+jasonrenau@users.noreply.github.com>
2024-01-14 20:12:32 +0100

    Add ToggleButton and toast notification for admin mode

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,13 +1,29 @@
-import AdminButton from "../pages/order/AdminButton";
+import { useState } from "react";
 import Profile from "../pages/order/Profile";
 import NavbarLogo from "./NavbarLogo";
+import ToggleButton from "./ToggleButton";
+import { toast } from "sonner";
 
 export default function Navbar({ userName }) {
+  const [isModeAdmin, setIsModeAdmin] = useState(false);
+
+  const displayToastNotification = () => {
+    if (!isModeAdmin) {
+      toast.success("Mode admin activé");
+    }
+    setIsModeAdmin(!isModeAdmin);
+  };
+
   return (
     <nav className="flex w-full justify-between p-3 shadow-md">
       <NavbarLogo />
       <div className="flex items-center gap-5">
-        <AdminButton />
+        <ToggleButton
+          labelIfUnchecked="ACTIVER LE MODE ADMIN"
+          labelIfChecked="DESACTIVER LE MODE ADMIN"
+          onToggle={displayToastNotification}
+          isChecked={isModeAdmin}
+        />
         <Profile userName={userName} />
       </div>
     </nav>
