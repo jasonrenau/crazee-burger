de4df6bbf5e7d8e2e6a855245cc2cfbbe0df2e63
Jason.renault <55652225+jasonrenau@users.noreply.github.com>
2024-01-14 13:15:23 +0100

    Update OrderPage component with menu and product cards

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,17 +1,25 @@
 import { useParams } from "react-router-dom";
 import Navbar from "../../ui/Navbar";
+import { fakeMenu2 } from "../../../utils/fakeMenu";
+import ProductCard from "./ProductCard";
+import { useState } from "react";
 
 export default function OrderPage() {
   const params = useParams();
+  const [menu, setMenu] = useState(fakeMenu2);
 
   return (
-    <div className="flex h-screen items-center justify-center bg-orange-500">
-      <div className="flex h-[95vh] w-[95%] flex-col rounded-lg ">
-        <header className=" rounded-t-lg bg-white">
+    <div className="flex h-[100vh] items-center justify-center  bg-orange-500 ">
+      <div className="h-[95%] w-[95%] overflow-hidden rounded-lg py-5">
+        <header className="rounded-t-lg bg-white">
           <Navbar userName={params.name} />
         </header>
-        <main className="h-full flex-1 rounded-b-lg bg-slate-50 shadow-[inset_0px_20px_20px_10px_#00000024]">
-          mlklmk
+        <main className="h-full overflow-y-scroll rounded-b-lg bg-slate-50 p-10 shadow-[inset_0px_20px_20px_10px_#00000024] ">
+          <div className="grid-auto-fit grid gap-5 pb-10">
+            {menu.map((product) => (
+              <ProductCard key={product.id} product={product} setMenu={setMenu} />
+            ))}
+          </div>
         </main>
       </div>
     </div>
